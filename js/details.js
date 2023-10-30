98e260ad8d52c6a38a62f6e489176ee28af1f006
“damian_jaraba” <damianjaraba@gmail.com>
2023-10-30 15:49:05 -0500

    Se generan los check box dinamicamnete y se les agrega funcionalidad para poder filtrar

---
+++ js/details.js
@@ -0,0 +1,45 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const arrayEvents = data.events;
+let detailsCard = arrayEvents.find((event) => event._id === id);
+
+// Función que genera las Cards de DETAILS
+const containerDetails = document.getElementById("containerDetails");
+
+function createCard(arrayEvent) {
+  let assistance 
+
+    if (arrayEvent.assistance !== undefined) {
+      assistance = `<p class="card-text"><span class="p-2 rounded  bg-warning">Assistance:</span> ${arrayEvent.assistance}</p>`;
+    } else {
+      assistance = `<p class="card-text"><span class=" p-2 rounded bg-success">Estimate :</span> ${arrayEvent.estimate}</p>`;
+    }
+  
+  const cardDetails = document.createElement("div");
+  cardDetails.innerHTML = `
+    <div class="card p-2 bg-dark-subtle">
+          <div class="row no-gutters">
+            <div class="col-md-6">
+              <img src="${arrayEvent.image}"
+                class="card-img h-100 w-100 img-fluid size-image" alt="${arrayEvent.name}">
+            </div>
+            <div class="col-md-6 ">
+              <div class="card-body border border-primary rounded border-2">
+                <h5 class="text-white bg-info text-color d-flex justify-content-center p-1 rounded-3 mb-2 fs-2 text-uppercase shadow mb-4">${arrayEvent.name}</h5>
+                <p class="card-text"><strong>Category:</strong> ${arrayEvent.category}</p>
+                <p class="card-text"><strong>Description:</strong> ${arrayEvent.description}.</p>
+                <p class="card-text"><strong>Date:</strong> ${arrayEvent.date}</p>
+                <p class="card-text"><strong>Place:</strong> ${arrayEvent.place}</p>
+                <p class="card-text"><strong>Capacity:</strong> ${arrayEvent.capacity}</p>
+                ${assistance}
+                <p class="card-text"><strong>Price:</strong> $ ${arrayEvent.price}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+
+  containerDetails.appendChild(cardDetails);
+}
+createCard(detailsCard);
--- js/upcomingEvents.js
+++ js/upcomingEvents.js
@@ -1,30 +1,108 @@
 const arrayEvents = data.events;
 const currentDate = data.currentDate;
+
 const arrayUpcomingEvents = [];
+
 for (let event of arrayEvents) {
   if (event.date > currentDate) {
     arrayUpcomingEvents.push(event);
   }
 }
 
-const containerCards = document.getElementById("container__cards");
+const containerCards = document.getElementById("containerCards");
+
+function preventRefresh(arrayEvents) {
+  arrayEvents.preventDefault();
+}
+
+let arrayCheck = [];
+arrayCheck = Array.from(
+  new Set(arrayEvents.map((arrayCategory) => arrayCategory.category))
+);
+
+//Array para guardar las categorias
+let categories = [];
+
+categories = Array.from(new Set(arrayEvents.map((event) => event.category)));
+
+categories.forEach((category) => {
+  let divCheckBox = document.createElement("div");
+  divCheckBox.classList.add("form-check", "form-check-inline");
+  divCheckBox.innerHTML = `
+      <input class="form-check-input" type="checkbox" id=${category} value='${category}'>
+      <label class="form-check-label text-light" for=${category}>${category}</label>
+  `;
+  containerCheckBox.appendChild(divCheckBox);
+});
+
+function filterCategory(arrayEvents) {
+  let checked = Array.from(
+    document.querySelectorAll("input[type=checkbox]:checked")
+  ).map((checkbox) => checkbox.value);
+
+  let filteredEvents = [];
+  arrayEvents.forEach((event) => {
+    checked.forEach((category) => {
+      if (category == event.category) {
+        filteredEvents.push(event);
+      }
+    });
+  });
+  if (filteredEvents.length === 0) {
+    return arrayEvents;
+  }
+  return filteredEvents;
+}
+
+filterCategory(arrayEvents);
+
+function filterText(arrayEvent) {
+  return arrayEvent.filter((event) =>
+    event.name.toLowerCase().includes(inputSearch.value.toLowerCase())
+  );
+}
+
+// Función que genera las Cards de UPCOMING EVENTS
 
 function createCard(arrayEvent) {
-  for (const event of arrayEvent) {
-    const card = document.createElement("div");
-    // card.className = "card card-items p-2";
-    card.classList.add("card", "card-items", "p-2");
-    card.innerHTML = `<img src="${event.image}" class="card-img-top rounded " alt="${event.name}">
+  if (arrayEvent.length === 0) {
+    containerCards.innerHTML = `
+    😨🤔🤔 ¡Oops! It seems we didn't find any results. Can you try again? 🤔🤔🤔
+  `;
+  } else {
+    containerCards.innerHTML = "";
+
+    for (const event of arrayEvent) {
+      const card = document.createElement("div");
+      card.className = "card card-items p-2";
+      card.innerHTML = `
+    <img src="${event.image}" class="card-img-top rounded " alt="${event.name}">
     <div class="card-body">
     <h5 class="card-title">${event.name}</h5>
     <p class="card-text">${event.description}</p>
     <div class="d-flex justify-content-between align-items-center">
-    <p class="m-0"><strong>Price:</strong> $ ${event.price}</p>
-    <a href="/pages/details.html" class="btn btn-primary"><strong>Details</strong></a>
+    <p class="m-0"><strong>Price:</strong> $${event.price} USD</p>
+    <a href="../pages/details.html?id=${event._id}" class="btn btn-primary"><strong>Details</strong></a>
     </div>
     </div>`;
 
-    containerCards.appendChild(card);
+      containerCards.appendChild(card);
+    }
   }
 }
 createCard(arrayUpcomingEvents);
+
+function superFilter(arrayUpcomingEvents) {
+  let filter = filterCategory(arrayUpcomingEvents);
+  let filter2 = filterText(filter);
+
+  createCard(filter2);
+}
+
+containerCheckBox.addEventListener("change", () => {
+  superFilter(arrayUpcomingEvents);
+});
+
+inputSearch.addEventListener("keyup", () => {
+  superFilter(arrayUpcomingEvents);
+});
